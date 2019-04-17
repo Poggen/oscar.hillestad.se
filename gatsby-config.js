200915fb2a4af242bf618522fe93fd041bb527cd
Oscar Hillestad <oscarhillestad@gmail.com>
2019-04-17 23:14:55 +0200

    Remove boilerplate values from config

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,13 +18,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `oscarhillestad`,
+        short_name: `oscar`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#fdbb2d`,
+        theme_color: `#fdbb2d`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/oscar.png`,
       },
     },
     `gatsby-plugin-sass`,
